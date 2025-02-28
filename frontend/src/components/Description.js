0e7db197b953937595219f352ba221805deadc48
huzaifagujjar678 <alihuzaifagujjar@gmail.com>
2025-02-28 21:17:29 +0500

    Update Description.js

--- frontend/src/components/Description.js
+++ frontend/src/components/Description.js
@@ -1,40 +1,53 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect ,useState } from 'react';
+import { useParams} from 'react-router-dom';
+import axios from 'axios';
 import './Desc.css'
 
 export default function Description() {
-
-    // use effect hook is use to access data from parent in link tag
-    const location = useLocation();
-    console.log(location.state);  // Check if the state is being passed correctly
-
-    // if it receive data from the BookSection.js than this data will destructur here
-    const { book, bookname, author, description, price, image } = location.state || {
-        // if there no data received from the booksction than this will be used as deafult
-        bookname: "Unknown",
-        author: "Unknown",
-        description: "No description available",
-        price: "N/A",
-        image: "https://tse3.mm.bing.net/th?id=OIP.zGwmhOTZXAWneFulgez-xAHaHa&pid=Api&P=0&h=180"
-    }; // Get the book from state
-    console.log(book)
+    const {id} = useParams();
+    const [data, setData] = useState()
+    // it is call automatically afer a component rerender
+    useEffect(() => {
+      const fetch = async () => {
+        // wait until data is fetchedfrom backend
+        await axios.get(`http://localhost:3001/api/v1/get/${id}`)
+          .then((res) => {
+            // data we set from book is asign to dta varable in usestate
+            setData(res.data.books);
+          })
+          .catch((err) => {
+            // if thereamy error it run
+            console.error("Error fetching books:", err);
+          });
+      };
+  
+      fetch();
+    }, [id]); // Added empty dependency array to run only once
+    console.log(data)
     return (
-        // main container
-        <div className="container  mt-5 p-5 pb-0 mb-0 main-cont" >
-            {/* first container for image */}
-            <div className="d-flex flex-wrap m-auto">
-                <div className="d-flex justify-content-center align-items-center m-auto img-cont">
-                    <img src={image} alt={bookname} className='img' />
-                </div>
-
-               {/* second container for other details abot book */}
-                <div className=' py-3 m-auto text-white customStyle'>
-                    <h2 className='bookheading'>BookName : {bookname}</h2>
-                    <p className='mt-4 mb-2 author'><strong>Author:</strong> {author}</p>
-                    <p className='price'><strong>Price :</strong> Rs. {price}</p>
-                    <p><strong className='desc-heading'>Description : </strong>{description}</p>
+        data ? (
+            <div className="container mt-5 p-5 pb-0 mb-0 main-cont">
+                <div className="d-flex flex-wrap m-auto">
+                    <div className="d-flex justify-content-center align-items-center m-auto img-cont">
+                        {/* Added a check to make sure that 'data.image' exists */}
+                        <img src={data.image || ''} alt={data.bookname || 'Book'} className="img" />
+                    </div>
+                    <div className="py-3 m-auto text-white customStyle" style={{maxWidth:'400px'}}>
+                        {/* Updated the text interpolation for the book name */}
+                        <h2 className="bookheading">BookName : {data.bookname}</h2>
+                        <p className="mt-4 mb-2 author"><strong>Author : </strong> {data.author}</p>
+                        {/* Ensured the price is correctly formatted */}
+                        <p className="price"><strong>Price :</strong> Rs. {data.price}</p>
+                        {/* Updated to display the description correctly */}
+                        <p><strong className="desc-heading">Description : </strong>{data.description}</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        ) : (
+            // Corrected the empty fragment to render nothing
+            <></>
+        )
     );
+    
 }
+
